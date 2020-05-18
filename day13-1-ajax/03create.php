48e998878f41d37d19ab46961a747adaf2469c1a
cxymds <cxymds@qq.com>
2020-05-18 11:45:01 +0800

    ajax前后端交互

---
+++ day13-1-ajax/03create.php
@@ -0,0 +1,33 @@
+<?php 
+$num = (int)$_GET['num'];
+//至少产生1名学生
+if($num<=0){
+	$re= [
+		'code' => 1001,
+		'message'=>'至少产生一名学生'
+	];
+	echo json_encode($re);
+	die;
+}
+//所有的学生
+$stu_arr = explode('|',file_get_contents('stu.txt'));
+//in_array  判断是否在一个数组中
+//mt_rand 产生随机数
+$arr = [];  //用户保存学生的编号
+for($i=0;$i<$num;$i++){ //用于循环产生学生编号
+	$stu_num = mt_rand(0,53); //随机产生
+	while(in_array($stu_num,$arr)){ //判断是否产生了相同的学生编号
+		$stu_num = mt_rand(0,52);;
+	}
+	array_push($arr,$stu_num);
+}
+
+$re = []; //返回的学生数组
+foreach($arr as $k){
+	array_push($re,$stu_arr[$k]);
+}
+echo json_encode($re);
+
+
+
+
--- day13-1-ajax/postData.php
+++ day13-1-ajax/postData.php
@@ -1,8 +1,9 @@
 <?php 
+
 //后端验证
 //1. 所有的信息都得有
 //检测用户名是否有
-if(!isset($_GET['user_name']) || empty($_GET['user_name'])){
+if(!isset($_POST['user_name']) || empty($_POST['user_name'])){
 	$return = [
 		"code"  =>1001,
 		"message" =>'用户名不能为空',
@@ -11,7 +12,7 @@ if(!isset($_GET['user_name']) || empty($_GET['user_name'])){
 	die;
 }
 //符合规则吗-长度不能少于6位
-if(strlen($_GET['user_name'])<6){
+if(strlen($_POST['user_name'])<6){
 	$return = [
 		'code'=> 2001,
 		'message'=> '用户名至少6位'
@@ -20,7 +21,7 @@ if(strlen($_GET['user_name'])<6){
 	die;
 }
 //用户名不能以数字开头
-if((int)$_GET['user_name']>0 || substr($_GET['user_name'],0,1)=='0'){
+if((int)$_POST['user_name']>0 || substr($_POST['user_name'],0,1)=='0'){
 	$return = [
 		'code'=> 2003,
 		'message'=> '用户名不能用数字开头'
@@ -31,7 +32,7 @@ if((int)$_GET['user_name']>0 || substr($_GET['user_name'],0,1)=='0'){
 
 //检测用户名是否存在  数据库查询
 $arr = ['ergouzi','wangmazi','liuxi','shichengkai','liulian','xiaoxiannv'];
-if(in_array($_GET['user_name'],$arr)){
+if(in_array($_POST['user_name'],$arr)){
 	$return = [
 		'code'=>3001,
 		'message'=>'用户名已存在'
@@ -43,7 +44,7 @@ if(in_array($_GET['user_name'],$arr)){
 // 用户名有要求吗？ 能重复吗  用户名注册机制的要求
 
 //2. 邮箱是不是邮箱 邮箱的合法性 ，正则
-if(!isset($_GET['email']) || empty($_GET['email'])){
+if(!isset($_POST['email']) || empty($_POST['email'])){
 	$return = [
 		"code"  =>1002,
 		"message" =>'邮箱不能为空',
@@ -52,7 +53,7 @@ if(!isset($_GET['email']) || empty($_GET['email'])){
 	die;
 }
 //3. 密码符合要求吗 
-if(!isset($_GET['pass']) || empty($_GET['pass'])){
+if(!isset($_POST['pass']) || empty($_POST['pass'])){
 	$return = [
 		"code"  =>1003,
 		"message" =>'密码不能为空',
@@ -63,9 +64,9 @@ if(!isset($_GET['pass']) || empty($_GET['pass'])){
 
 //注册
 
-$user = $_GET['user_name'];
-$email = $_GET['email'];
-$pass = $_GET['pass'];
+$user = $_POST['user_name'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
 
 //保存数据库
 $str = $user.'|'.$email.'|'.$pass.'#';
