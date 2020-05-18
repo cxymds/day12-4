c733a73fb675e099cff0f5b6912e41941123d663
cxymds <cxymds@qq.com>
2020-05-18 09:53:48 +0800

    get ajax讲解结束。主要实现ajaxget方式注册

--- day13-1-ajax/getData.php
+++ day13-1-ajax/getData.php
@@ -4,27 +4,62 @@
 //1. 所有的信息都得有
 //检测用户名是否有
 if(!isset($_GET['user_name']) || empty($_GET['user_name'])){
-	die('请输入正确的用户名');
+	$return = [
+		"code"  =>1001,
+		"message" =>'用户名不能为空',
+	];
+	echo json_encode($return);
+	die;
 }
 //符合规则吗-长度不能少于6位
 if(strlen($_GET['user_name'])<6){
-	die('长度不合格，请输入至少6位长度的用户名');
+	$return = [
+		'code'=> 2001,
+		'message'=> '用户名至少6位'
+	];
+	echo json_encode($return);
+	die;
 }
+//用户名不能以数字开头
+if((int)$_GET['user_name']>0 || substr($_GET['user_name'],0,1)=='0'){
+	$return = [
+		'code'=> 2003,
+		'message'=> '用户名不能用数字开头'
+	];
+	echo json_encode($return);
+	die;
+}
+
 //检测用户名是否存在  数据库查询
 $arr = ['ergouzi','wangmazi','liuxi','shichengkai','liulian','xiaoxiannv'];
 if(in_array($_GET['user_name'],$arr)){
-	die('用户名已经存在');
+	$return = [
+		'code'=>3001,
+		'message'=>'用户名已存在'
+	];
+	echo json_encode($return);
+	die;
 }
 
 // 用户名有要求吗？ 能重复吗  用户名注册机制的要求
 
 //2. 邮箱是不是邮箱 邮箱的合法性 ，正则
 if(!isset($_GET['email']) || empty($_GET['email'])){
-	die('请输入正确的邮箱地址');
+	$return = [
+		"code"  =>1002,
+		"message" =>'邮箱不能为空',
+	];
+	echo json_encode($return);
+	die;
 }
 //3. 密码符合要求吗 
 if(!isset($_GET['pass']) || empty($_GET['pass'])){
-	die('请输入密码');
+	$return = [
+		"code"  =>1003,
+		"message" =>'密码不能为空',
+	];
+	echo json_encode($return);
+	die;
 }
 
 //注册
@@ -37,4 +72,8 @@ $pass = $_GET['pass'];
 $str = $user.'|'.$email.'|'.$pass.'#';
 file_put_contents('user.txt',$str,FILE_APPEND);
 //返回成功
-echo '注册成功';
+$return= [
+	'code' =>200,
+	'message' =>"注册成功"
+];
+echo json_encode($return);
